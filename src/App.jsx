a3cea8a97a1952b09f7d1b8ce8f49aa524380d12
Nabiel Utomo <nabielutomo@gmail.com>
2025-05-01 23:13:14 +0700

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import Navbar from './Navbar';
-import CounterDemo from './CounterDemo';
-import Card1 from './Guitarist';
-import Card2 from './Drummer';
-import Card3 from './Bassist';
-import Card4 from './Keyboardist';
-import Card5 from './Singer';
+import { Button } from "@/components/ui/button"
 
-const App = () => {
-    return (
-        <div>
-            <Navbar />
-            <CounterDemo />
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-20 px-4">
-                <Card1 />
-                <Card2 />
-                <Card3 />
-                <Card4 />
-                <Card5 />
-            </div>
-        </div>
-    );
-};
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  )
+}
 
-export default App;
+export default App
